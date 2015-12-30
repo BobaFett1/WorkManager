72bc891d89c449b15c64e0be790e476cb66d6553
BobaFett1 <michelehmen@hotmail.de>
2015-12-30 19:26:55 +0100

    Diverses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230171258) do
+ActiveRecord::Schema.define(version: 20151230171319) do
 
   create_table "customers", force: :cascade do |t|
     t.integer  "Number"
